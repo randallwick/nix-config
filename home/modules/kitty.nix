3998c1656ae1a7aec2c6a205b75d3d55d6b517f4
Randall Wick <randy.wick@gmail.com>
2024-09-05 22:45:57 -0500

    kitty settings

--- home/modules/kitty.nix
+++ home/modules/kitty.nix
@@ -9,6 +9,14 @@ in {
 			size = 14;
 		};
 
+    settings = {
+      background_opacity = "0.7";
+      copy_on_select = "yes";
+      enable_audio_bell = "no";
+      shell = "zsh";
+      editor = "nvim";
+    };
+
 		shellIntegration = {
 			enableZshIntegration = true;
 		};
