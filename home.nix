1919b1418456024e905d0406fafaeb3de0a78d54
Randall Wick <randy.wick@gmail.com>
2024-09-05 22:25:10 -0500

    remove modules from home.nix

--- home.nix
+++ home.nix
@@ -70,66 +70,6 @@
     usbutils
   ];
 
-  programs.git = {
-    enable = true;
-    userName = "Randall Wick";
-    userEmail = "randy.wick@gmail.com";
-    extraConfig = {
-      init.defaultBranch = "main";
-    };
-    aliases = {
-      s = "status";
-    };
-  };
-
-  programs.starship = {
-    enable = true;
-  };
-
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      env.TERM = "xterm-256color";
-      font = {
-        size = 14;
-      };
-      colors.draw_bold_text_with_bright_colors = true;
-      scrolling.multiplier = 5;
-      selection.save_to_clipboard = true;
-    };
-  };
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      ls = "eza";
-      ll = "eza -lah";
-      # vim = "nvim";
-      # vi = "nvim";
-      update = "sudo nixos-rebuild switch";
-      ".." = "cd ..";
-      "..." = "cd ../..";
-      "...." = "cd ../../..";
-      "....." = "cd ../../../..";
-      mkdir = "mkdir -p";
-    };
-
-    history = {
-      size = 10000;
-    };
-
-    zplug = {
-      enable = true;
-      plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; } 
-      ];
-    };
-  };
-
   home.stateVersion = "24.05";
 
   programs.home-manager.enable = true;
