ed50726c33481019529b78144ac181a03f942088
Randall Wick <randy.wick@gmail.com>
2024-09-05 23:53:18 -0500

    quiet boot

--- configuration.nix
+++ configuration.nix
@@ -40,7 +40,14 @@
     kernelParams = [
       "quiet"
       "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
     ];
+
+    loader.timeout = 0;
   };
 
   networking.hostName = "stilicho"; # Define your hostname.
