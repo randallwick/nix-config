9b5698c02ba8b6049bc83bfca6fe128e96509007
Randall Wick <randy.wick@gmail.com>
2024-09-12 00:53:13 -0500

    override home manager monospace font

--- home/modules/nerdfonts.nix
+++ home/modules/nerdfonts.nix
@@ -1,8 +1,16 @@
 {pkgs, ...}: let
   example = "thing";
 in {
-  fonts.fontconfig.enable = true;
 	home.packages = with pkgs; [
 		(nerdfonts.override { fonts = [ "FiraCode" ]; })
 	];
+
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      monospace = [
+        "FiraCode Nerd Font Mono"
+      ];
+    };
+  };
 }
