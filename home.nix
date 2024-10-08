b2d751bef9d78997e9f6539e986d6d7656aaf9fb
Randall Wick <randy.wick@gmail.com>
2024-10-08 09:05:00 -0500

    move non-user sops config to home.nix

--- home.nix
+++ home.nix
@@ -1,9 +1,24 @@
-{ config, pkgs, ... }:
-
+{ config, inputs, pkgs, ... }: let
+  secretspath = builtins.toString inputs.nix-secrets;
+in
 {
   imports = [
     ./home/modules/common.nix
+    inputs.sops-nix.homeManagerModules.sops
   ];
+
+  sops = {
+    defaultSopsFile = "${secretspath}/secrets.yaml";
+    defaultSopsFormat = "yaml";
+    defaultSymlinkPath = "/run/user/1000/secrets";
+    defaultSecretsMountPoint = "/run/user/1000/secrets.d";
+    age.keyFile = "/home/randall/.config/sops/age/keys.txt";
+
+    secrets = {
+      aws_access_key_id = { };
+      aws_secret_access_key = { };
+    };
+  };
 
   home.username = "randall";
   home.homeDirectory = "/home/randall";
