82dc67e145d6af5625934e180614f5972c9f64d5
Randall Wick <randy.wick@gmail.com>
2024-10-07 23:37:37 -0500

    enable hyprland

---
+++ home/modules/hyprland.nix
@@ -0,0 +1,4 @@
+{ pkgs, ... }:
+{
+  wayland.windowManager.hyprland.enable = true;
+}
--- machines/stilicho/configuration.nix
+++ machines/stilicho/configuration.nix
@@ -86,6 +86,8 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  programs.hyprland.enable = true;
+
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
